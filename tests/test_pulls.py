c7e78f2a01f638e11d0aff1a513e4ba798bb3067
Dzmitry_Kramich <Dzmitry_Kramich@epam.com>
2020-09-28 00:23:07 +0300

    fixed style issue

--- tests/test_pulls.py
+++ tests/test_pulls.py
@@ -10,11 +10,13 @@ class TestPulls(TestCase):
 
     def test_get_pulls_all(self):
         """Test checking return value when empty state"""
-        self.assertEqual(pulls.get_pulls(None), testJson.resultAll)
+        self.assertEqual(pulls.get_pulls(None),
+                         testJson.resultAll)
 
     def test_get_pulls_state(self):
         """Test checking return value when not empty state"""
-        self.assertEqual(pulls.get_pulls("open"), testJson.resultOpenAccepted)
+        self.assertEqual(pulls.get_pulls("open"),
+                         testJson.resultOpenAccepted)
 
     def test_get_git_json(self):
         """Test data received"""
@@ -22,17 +24,22 @@ class TestPulls(TestCase):
 
     def test_get_data_state(self):
         """Test states"""
-        self.assertEqual(pulls.get_data_state(testJson.json, "open"), testJson.resultOpenAccepted)
-        self.assertEqual(pulls.get_data_state(testJson.json, "closed"), testJson.resultClosedNeedWork)
+        self.assertEqual(pulls.get_data_state(testJson.json, "open"),
+                         testJson.resultOpenAccepted)
+        self.assertEqual(pulls.get_data_state(testJson.json, "closed"),
+                         testJson.resultClosedNeedWork)
 
     def test_get_data_labels(self):
         """Test states"""
-        self.assertEqual(pulls.get_data_labels(testJson.json, "accepted"), testJson.resultOpenAccepted)
-        self.assertEqual(pulls.get_data_labels(testJson.json, "needs work"), testJson.resultClosedNeedWork)
+        self.assertEqual(pulls.get_data_labels(testJson.json, "accepted"),
+                         testJson.resultOpenAccepted)
+        self.assertEqual(pulls.get_data_labels(testJson.json, "needs work"),
+                         testJson.resultClosedNeedWork)
 
     def test_get_all_data(self):
         """Test get all data"""
-        self.assertEqual(pulls.get_all_data(testJson.json), testJson.resultAll)
+        self.assertEqual(pulls.get_all_data(testJson.json),
+                         testJson.resultAll)
 
     def tearDown(self):
         """Finish"""
